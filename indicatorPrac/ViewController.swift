e67762c9f3803f99899a254ba1a38eb5aebdad43
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-01 15:19:24 +0900

    커스텀 인디케이터 구현

--- indicatorPrac/ViewController.swift
+++ indicatorPrac/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NVActivityIndicatorViewable {
 
+    @IBAction func button(_ sender: UIButton) {
+        startAnimating(CGSize(width: 30, height: 30), message: "Loading...", type: NVActivityIndicatorType.cubeTransition)
+        //버튼 클릭시 cubeTransition효과의 Indicator와 함께 Loading 메시지가 출력된다.
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5){
+            NVActivityIndicatorPresenter.sharedInstance.setMessage("Authenticating")
+            //1.5초뒤 메시지는 Authenticating으로 변경
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+3){
+            self.stopAnimating()
+            self.performSegue(withIdentifier: "nextPage", sender: nil)
+            //3초가 된 순간 PerformSegue를 통해 화면이 전환되고 Indicator 애니메이션은 중단된다.
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
